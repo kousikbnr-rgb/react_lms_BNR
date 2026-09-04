30dc9151756905472f92a0ee53422c66114626c4
jithesh17 <jitheshraju123@gmail.com>
2026-09-04 12:44:59 +0530

    update done
    added a comment

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Dashboard from "./pages/Dashboard";
 import Profile from "./pages/Profile"
 import Course from "./pages/Course"
 import MCQ from "./pages/MCQ";
-
+//hello update
 
 
 function App() {
